5c4cc79846096e2601ded8f7166020955cd5cc61
Nikolina Šušak <nsusak0@gmail.com>
2021-06-30 12:25:02 +0200

    added basic validation

--- client/src/components/users/UserCreate.js
+++ client/src/components/users/UserCreate.js
@@ -2,13 +2,23 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class UserCreate extends React.Component {
-    renderInput({ input, label }) {
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput = ({ input, label, type, meta }) => {
+        const className = `field ${meta.error && meta.touched ? "error" : ""}`
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} />
+                <input {...input} type={type} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
-
         )
     }
     onSubmit(formValues) {
@@ -16,16 +26,31 @@ class UserCreate extends React.Component {
     }
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-                <Field name="name" label="Enter a name" component={this.renderInput} />
-                <Field name="surname" label="Enter a surname" component={this.renderInput} />
-                <Field name="email" label="Enter an email" component={this.renderInput} />
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="name" label="Enter a name" type="text" component={this.renderInput} />
+                <Field name="surname" label="Enter a surname" type="text" component={this.renderInput} />
+                <Field name="email" label="Enter an email" type="email" component={this.renderInput} />
                 <button className="ui button primary">Submit</button>
             </form>
         )
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.name) {
+        errors.name = "You must enter a name"
+    }
+    if (!formValues.surname) {
+        errors.surname = "You must enter a surname"
+    }
+    if (!formValues.email) {
+        errors.email = "You must enter a valid email"
+    }
+    return errors;
+};
+
 export default reduxForm({
-    form: "userCreate"
+    form: "userCreate",
+    validate
 })(UserCreate);
