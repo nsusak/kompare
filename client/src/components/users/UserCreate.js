07173f2abb54a18cab9e194a41302b75dc6f1398
Nikolina Šušak <nsusak0@gmail.com>
2021-06-30 11:54:19 +0200

    showing a form with redux-form

--- client/src/components/users/UserCreate.js
+++ client/src/components/users/UserCreate.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const UserCreate = () => {
-    return <div>UserCreate</div>
+class UserCreate extends React.Component {
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+
+        )
+    }
+    onSubmit(formValues) {
+        console.log(formValues)
+    }
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="name" label="Enter a name" component={this.renderInput} />
+                <Field name="surname" label="Enter a surname" component={this.renderInput} />
+                <Field name="email" label="Enter an email" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default UserCreate;
+export default reduxForm({
+    form: "userCreate"
+})(UserCreate);
